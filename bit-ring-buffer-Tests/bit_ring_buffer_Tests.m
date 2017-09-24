9826b18c56349281dd1590c804087f679f09ec3b
Jan <github@geheimwerk.de>
2017-09-24 11:03:04 +0200

    Initial commit

---
+++ bit-ring-buffer-Tests/bit_ring_buffer_Tests.m
@@ -0,0 +1,39 @@
+//
+//  bit_ring_buffer_Tests.m
+//  bit-ring-buffer-Tests
+//
+//  Created by Jan on 24.09.17.
+//  Copyright © 2017 Jan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bit_ring_buffer_Tests : XCTestCase
+
+@end
+
+@implementation bit_ring_buffer_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ bit-ring-buffer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bit-ring-buffer
+//
+//  Created by Jan on 24.09.17.
+//  Copyright © 2017 Jan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
